bf4e9e00c1b79344801fde564acba96b22c0dd9c
Azizbek-py <azizbektolliboyev155@gmail.com>
2026-05-19 23:10:01 +0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ from details.handlers import *
 app = Application.builder().token(BOT_TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-# app.add_handler(CallbackQueryHandler(button))
+app.add_handler(CallbackQueryHandler(callback_query_handler))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text))
 
 if __name__ == "__main__":
